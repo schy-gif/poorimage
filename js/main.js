2dc9ed927415292437a395418cee9d7cbe9637f0
schy-gif <schy-gif@users.noreply.github.com>
2020-12-13 22:25:37 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+    // FIX THIS . PUT IT IN ITS OWN FILE
+     
+
+
+
+
+    //FIX THIS . PUT IT IN ITS OWN FILE
+
+
+$("#layout1__s1").hover(function(){
+    $(".hover-on-1").addClass("active");
+// }, function(){
+//     $(".hover-on-1").removeClass("active");
+})
+
+$("#box1").hover(function(){
+    $(this).addClass("open");
+}, function(){
+    $(".hover-on-1").removeClass("active");
+})
+
+
+
+
+$("#layout1__s2").hover(function(){
+    $(".hover-on-2").addClass("active");
+}, function(){
+    $(".hover-on-2").removeClass("active");
+})
+
+
+
+$("#layout1__s3").hover(function(){
+    $(".hover-on-3").addClass("active");
+}, function(){
+    $(".hover-on-3").removeClass("active");
+})
+
+$("#layout1__s4").hover(function(){
+    $(".hover-on-4").addClass("active");
+}, function(){
+    $(".hover-on-4").removeClass("active");
+})
+
+$("#layout1__s5").hover(function(){
+    $(".hover-on-5").addClass("active");
+}, function(){
+    $(".hover-on-5").removeClass("active");
+})
+
+$("#layout1__s6").hover(function(){
+    $(".hover-on-6").addClass("active");
+}, function(){
+    $(".hover-on-6").removeClass("active");
+})
+// console.log("testing")
+// $(".menu").click(function(){
+// 	$(this).toggleClass("open")
+// })
+
+
+
+// $(".double").hover(function(){
+// 	$(".another").addClass ("shapeShift")
+		
+// },function(){
+// 	$(".another").removeClass ("shapeShift")
+		
+// })
